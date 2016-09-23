a4bb2e10759329a1f4d5a8f0de45e70d4a981ded
alex <boomscrip@gmail.com>
2016-09-23 14:32:10 +0300

    chat

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var submit = document.getElementsByClassName('submit')[0]; // submit button
 var input = document.getElementsByClassName('form-control')[0]; // message field
+var ul = document.getElementsByClassName('message-list')[0];
 
-
-submit.onclick = function () {
+submit.onclick = function (e) {
+	e.preventDefault();
 	console.log(input.value)
+
+	var li = document.createElement('li');
+	li.innerHTML = input.value;
+	ul.appendChild(li);
+	input.value = '';
 };
-var li = document.createElement('li');
